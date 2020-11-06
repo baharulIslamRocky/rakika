411a9ddedeb59e7ce6d3c6acd485233ea026ee6e
Ghurii <rockey.huawei@gmail.com>
2020-11-06 20:04:35 +0600

    greating method added to hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,9 @@
 public class hello{
     public static void main(String[] args){
         System.out.prinntln('writing java code after so long time');
+        greatting('Rockey');
     }
+}
+public static void greatting(String a){
+    System.out.prinntln('hello '+a);
 }
