4ec6167616de5af4eb3a0b85988f7d530cf7addf
Ghurii <rockey.huawei@gmail.com>
2020-11-08 19:01:03 +0600

    hell0 java got square fn to ghurii branch

--- hello.java
+++ hello.java
@@ -1,10 +1,14 @@
 
 public class hello{
     public static void main(String[] args){
-        System.out.prinntln('writing java code after so long time');
-        greatting('Rockey');
+        System.out.prinntln("writing java code after so long time");
+        greatting("Rockey");
     }
 }
 public static void greatting(String a){
-    System.out.prinntln('hello '+a);
+    System.out.prinntln("hello "+a);
+}
+//this is a square method
+public static double square(in a){
+    return a*a;
 }
