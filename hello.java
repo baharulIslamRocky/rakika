effed8904e51155ec2249ace53a18ad52b08533f
Ghurii <rockey.huawei@gmail.com>
2020-12-27 20:00:30 +0600

    add a method to hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,13 @@
 
 public class hello{
     public static void main(String[] args){
-        System.out.prinntln('writing java code after so long time');
-        greatting('Rockey');
+        System.out.println("writing java code after so long time");
+        greatting("Rockey");
     }
 }
 public static void greatting(String a){
-    System.out.prinntln('hello '+a);
+    System.out.println(a);
+}
+public static double add(int a ,int b){
+    return a+b;
 }
